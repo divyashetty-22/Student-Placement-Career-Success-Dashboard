b5b2e77ba8e509667ab6443bd7889c8622164a8b
DIVYA <22.divyamahadeva@gmail.com>
2026-06-04 16:03:49 +0530

    Create  app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Student Placement Dashboard",
+    page_icon="🎓",
+    layout="wide"
+)
+
+st.title("🎓 Student Placement & Career Success Dashboard")
+
+st.markdown("""
+### Features
+
+- Dataset Overview
+- Placement Analysis
+- Salary Analysis
+- AI Skills Analysis
+- State & Branch Insights
+
+Use the sidebar to navigate.
+""")
